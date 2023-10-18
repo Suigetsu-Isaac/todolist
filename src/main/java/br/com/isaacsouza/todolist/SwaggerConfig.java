ed81261d9f2441a08198f90b219e596fabcdfe2e
Suigetsu-Isaac <isaacantonioalves@gmail.com>
2023-10-18 17:37:17 -0300

    retirando o swagger

--- src/main/java/br/com/isaacsouza/todolist/SwaggerConfig.java
+++ src/main/java/br/com/isaacsouza/todolist/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package br.com.isaacsouza.todolist;
-
+/**
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,10 +14,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();
+        return new Docket(springfox.documentation.spi.DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("br.com.isaacsouza.todolist")) // Substitua pelo pacote onde estão seus controladores
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("API do Meu Aplicativo")
+            .description("Documentação da API do Meu Aplicativo")
+            .version("1.0")
+            .contact(new Contact("Seu Nome", "", "seu@email.com"))
+            .build();
     }
 }
+ */
